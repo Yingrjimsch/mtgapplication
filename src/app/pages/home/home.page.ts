2738a745d6d8c80b090e5639250d4cbe2cafc919
Gabriel Nobel <nobel98@bluewin.ch>
2019-01-21 21:38:37 +0100

    starting with dbredesign

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -8,6 +8,7 @@ import { map } from 'rxjs/operators';
 import { CardService } from '../../services/dbservices/card.service';
 import { SettingsService } from '../../services/dbservices/settings.service';
 import { ActivatedRoute } from '@angular/router';
+import { FirestoreService } from 'src/app/services/dbservices/firestore.service';
 
 @Component({
   selector: 'app-home',
@@ -16,8 +17,13 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class HomePage {
 
-  constructor(private c: CardCollectionService, private c3: CardService) {
-    c.getCardsByCollection('jkm30pyPyqzufmB3PIYo')
-    .subscribe(c2 => console.log(c2));
+  constructor(private c: FirestoreService, private c3: CardService) {
+    let card: Card = new Card('test', 'xnxx', 'path');
+    card.id = '09987';
+    let deck: CardCollection = new CardCollection('TestDeck');
+    deck.id = 'JdT5KwAyo5Jd8OqRUDit';
+    deck.numberOfCards = 0;
+    c.addCardToDeck(deck, card);
+    // c.getCardsByDeckId('JdT5KwAyo5Jd8OqRUDit').subscribe(l => console.log(l))
   }
 }
---
+++ src/app/services/dbservices/firestore.service.ts
@@ -0,0 +1,77 @@
+import { Injectable } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/firestore';
+import { firestore } from 'firebase';
+import { map } from 'rxjs/operators';
+import { Card } from 'src/app/classes/card';
+import { CardCollection } from 'src/app/classes/card-collection';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class FirestoreService {
+
+  constructor(private firestore: AngularFirestore) { }
+
+  getDecksByUserId(id: string) {
+    return this.firestore.collection('decks', q => q.where('userId', '==', id)).snapshotChanges().pipe(
+      map(changes => changes.map(deck => ({ id: deck.payload.doc.id, ...deck.payload.doc.data() }))
+      )
+    );
+  }
+
+  getCardsByDeckId(id: string) {
+    return this.firestore.collection('decks').doc(id).collection('cards').snapshotChanges().pipe(
+      map(changes => changes.map(card => ({ id: card.payload.doc.id, ...card.payload.doc.data() }))
+      )
+    );
+  }
+
+  createDeck(userId: string, deckName: string) {
+    this.firestore.collection('decks').add({
+      userId: userId,
+      numberOfCards: 0,
+      deckName: deckName
+    })
+  }
+
+  deleteDeck(deckId: string) {
+    this.firestore.collection('decks').doc(deckId).delete();
+  }
+
+  addCardToDeck(deck: CardCollection, card: Card) {
+    const deckRef = this.firestore.collection('decks').doc(deck.id).ref;
+    const cardRef = deckRef.collection('cards').doc(card.id);
+    return this.firestore.firestore.runTransaction(async transaction => {
+      const c = await transaction.get(cardRef);
+      if (c.exists) {
+        card.numberOfCards++;
+      }
+      deck.numberOfCards++;
+      transaction.update(deckRef, { numberOfCards: deck.numberOfCards });
+      transaction.set(cardRef, Object.assign({}, card));
+    });
+  }
+
+  updateCard(deck: CardCollection, card: Card) {
+    const deckRef = this.firestore.collection('decks').doc(deck.id).ref;
+    const cardRef = deckRef.collection('cards').doc(card.id);
+    return this.firestore.firestore.runTransaction(async transaction => {
+      const c = await transaction.get(cardRef);
+      if(c.data()['number'] <= card.numberOfCards) {
+        deck.numberOfCards--;
+      }
+      transaction.update(deckRef, { numberOfCards: deck.numberOfCards });
+      transaction.update(cardRef, Object.assign({}, card));
+    });
+  }
+
+  removeCardsFromDeck(deck: CardCollection, cardId: string) {
+    const deckRef = this.firestore.collection('decks').doc(deck.id).ref;
+    const cardRef = deckRef.collection('cards').doc(cardId);
+    return this.firestore.firestore.runTransaction(async transaction => {
+      deck.numberOfCards--;
+      transaction.update(deckRef, { numberOfCards: deck.numberOfCards });
+      transaction.delete(cardRef);
+    });
+  }
+}
